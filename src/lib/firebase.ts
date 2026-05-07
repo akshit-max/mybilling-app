0cad424cfaf0af2946aa76eef7a8eed4b2b35c1d
Akshit Bhandula <akshitbhandula@gmail.com>
2026-05-07 21:41:16 +0530

    ready to deploy

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,16 +5,33 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBvoR7KSDMV7QQWJ1Ik5JgYTReWV2OXF_4",
-  authDomain: "billing-app-86a8e.firebaseapp.com",
-  projectId: "billing-app-86a8e",
-  storageBucket: "billing-app-86a8e.firebasestorage.app",
-  messagingSenderId: "966455162264",
-  appId: "1:966455162264:web:8c6bc4476864eee4d2e7ed"
-};
 
+
+const firebaseConfig = {
+  apiKey:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_API_KEY,
+
+  authDomain:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+
+  projectId:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+
+  storageBucket:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+
+  messagingSenderId:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+
+  appId:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 // // Initialize Firebase
 // export const app = initializeApp(firebaseConfig);
 // initialize app
